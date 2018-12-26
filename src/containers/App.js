c68a15854801cc9a78bf0d8c04248130e4216c89
YaroslavW <doctor355@yahoo.com>
2018-12-26 15:34:36 +0300

    Finish

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-
-// import './App.css';
-
-import { connect } from "react-redux"
+import './App.css';
+import User from '../components/User';
+import Year from '../components/Year';
+import setYearAction from '../actions/actionYear';
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
     return (
-          <h2>Welcome to React {this.props.user}</h2>
+      <div>
+        <User user={this.props.user} />
+        <Year year={this.props.year} setYear={this.props.setYearFunction} />
+      </div>
+          
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    user: state.userInfo.user
+    user: state.userInfo.user,
+    year: state.userInfo.year
   }
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    setYearFunction: year => {
+      dispatch(setYearAction(year))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
